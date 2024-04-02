4e2c7ea0a91f61482f4fe216c14b3b5f280e40c7
renan <renanlemos21@gmail.com>
2024-04-02 14:40:36 -0300

    feat: add layout spacing to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import AppRouter from './routes.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
+      {/*Layout spacing*/}
+      <Box p={16}>
         <AppRouter />
+      </Box>
     </ChakraProvider>
   )
 }
