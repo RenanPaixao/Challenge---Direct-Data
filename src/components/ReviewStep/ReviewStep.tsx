f3d6aadb3b74946ecfe80264423dc7552ed033f5
renan <renanlemos21@gmail.com>
2024-04-04 15:31:33 -0300

    feat: translate summary keys to portuguese and format birthday date

--- src/components/ReviewStep/ReviewStep.tsx
+++ src/components/ReviewStep/ReviewStep.tsx
@@ -4,25 +4,31 @@ import { Box, Button, Center } from '@chakra-ui/react'
 import { StepSummary } from './StepSummary.tsx'
 import { FormFooterWrapper } from '../FormFooterWrapper/FormFooterWrapper.tsx'
 import { StepperContext } from '../../context/StepperContext.tsx'
+import { DateTime } from 'luxon'
 
 export const ReviewStep = () => {
   const { goToPrevious } = useContext(StepperContext)!
   const { address, aboutYouInformation } = useContext(StudentContext)
+  const { responsible, ...aboutYouWithoutResponsible } = aboutYouInformation!
 
-  const { responsible, ...aboutYouInformationWithoutResponsible } = aboutYouInformation!
+  const aboutYouWithFormattedDate = formatBirthDate(aboutYouWithoutResponsible)
+  const responsibleWithFormattedDate = formatBirthDate(responsible ?? {})
 
   const sectionSteps = [
     {
       sectionTitle: 'Sobre Você',
-      sectionContent: aboutYouInformationWithoutResponsible,
+      sectionContent: mapObjectKeysToPortuguese(aboutYouWithFormattedDate),
       nested: [
         {
           sectionTitle: 'Responsável',
-          sectionContent: { ...responsible }
+          sectionContent: mapObjectKeysToPortuguese(responsibleWithFormattedDate)
         }
       ]
     },
-    { sectionTitle: 'Address', sectionContent: address !== null ? address : {} }
+    {
+      sectionTitle: 'Endereço',
+      sectionContent: mapObjectKeysToPortuguese(address ?? {})
+    }
   ]
 
   return <Center flexDirection={'column'}>
@@ -37,4 +43,48 @@ export const ReviewStep = () => {
       <Button>Avançar</Button>
     </FormFooterWrapper>
   </Center>
+}
+
+/**
+ * Map object keys to Portuguese.
+ * @param object
+ */
+function mapObjectKeysToPortuguese(object: Record<string, any>) {
+  const allKeys: Record<string, string> = {
+    name: 'Nome',
+    lastName: 'Sobrenome',
+    birthDate: 'Data de nascimento',
+    cpf: 'CPF',
+    weight: 'Peso',
+    height: 'Altura',
+    email: 'Email',
+    phone: 'Telefone',
+    responsibleName: 'Nome do responsável',
+    responsibleLastName: 'Sobrenome do responsável',
+    responsibleBirthDate: 'Data de nascimento do responsável',
+    responsibleCpf: 'CPF do responsável',
+    responsibleEmail: 'Email do responsável',
+    responsiblePhone: 'Telefone do responsável',
+    cep: 'CEP',
+    street: 'Rua',
+    number: 'Número',
+    complement: 'Complemento',
+    district: 'Bairro',
+    city: 'Cidade',
+    state: 'Estado'
+  }
+  return Object.keys(object).reduce((acc, key) => {
+    acc[allKeys[key]] = object[key]
+    return acc
+  }, {} as Record<string, string>)
+}
+
+/**
+ * Format the birthdate of an object.
+ */
+function formatBirthDate(object: Record<string, any>) {
+  return {
+    ...object,
+    birthDate: DateTime.fromISO(object.birthDate).toFormat('dd/MM/yyyy')
+  }
 }
