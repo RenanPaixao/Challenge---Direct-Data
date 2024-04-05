8be2678bf0a790b5bdd5b83361206f176b25240f
renan <renanlemos21@gmail.com>
2024-04-05 01:57:18 -0300

    feat: add scrollBar style

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import AppRouter from './routes.tsx'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import customTheme from './theme.ts'
 import { StudentProvider } from './context/StudentContext.tsx'
+import './assets/styles/main.css'
 
 function App() {
   return (
